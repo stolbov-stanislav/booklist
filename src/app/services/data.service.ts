c061c1c6360ea131c97346cf4e7f904087726e4b
Stanislav Stolbov <stolbovstanislav@mail.ru>
2024-02-26 17:24:29 +0000

    feat: add clearAll method to data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -80,62 +80,14 @@ export class DataService extends Dexie {
     }
   }
 
-@Injectable({
-  providedIn: 'root'
-})
-export class DataService extends Dexie {
-  public books!: Table<Book, number>;
-  public authors!: Table<Author, number>;
-
-  constructor() {
-    super('ngdexieliveQuery');
-    this.version(3).stores({
-      books: '++id',
-      authors: '++id, &name',
-    });
-    this.on('populate', () => this.populate());
-  }
-
-  async populate() {
-    await this.books.bulkAdd([
-      {
-        title: 'New event: Trip to Vegas',
-        author: 'Matt Chorsey',
-        pagesCount: 32,
-        language: 'English',
-        genre: 'fiction',
-      },
-      {
-        title: 'Lolita',
-        author: 'Nabokov',
-        pagesCount: 200,
-        language: 'Russian',
-        genre: 'fiction',
-      },
-      {
-        title: 'Deep into Universe',
-        author: 'Hawking',
-        pagesCount: 100,
-        language: 'English',
-        genre: 'non-fiction',
-      },
-      {
-        title: 'The old man and the sea',
-        author: 'Hemingway',
-        pagesCount: 70,
-        language: 'English',
-        genre: 'fiction',
-      },
-      {
-        title: 'The Great Gatsby',
-        author: 'Fitzgerald',
-        pagesCount: 220,
-        language: 'English',
-        genre: 'fiction',
-      },
-    ]);
-    
-    await this.books.each((book) => this.authors.add({name: book.author}));
+  public async clearAll() {
+    try {
+      await this.books.clear();
+      await this.authors.clear();
+    }
+    catch (e) {
+      throw new Error(`DataService clearAll error: ${e}`);
+    }
   }
 
   public getBooks(): Promise<Book[]> {
