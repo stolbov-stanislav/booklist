fac94696b94b8d553b530666078a5a6465217fbc
Stanislav Stolbov <stolbovstanislav@mail.ru>
2024-02-26 17:24:13 +0000

    feat: wrap data service async methods with try/catch

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -16,6 +16,70 @@ export interface Author {
   id?: number;
 }
 
+export const BOOKS = [
+  {
+    title: 'New event: Trip to Vegas',
+    author: 'Matt Chorsey',
+    pagesCount: 32,
+    language: 'English',
+    genre: 'fiction',
+  },
+  {
+    title: 'Lolita',
+    author: 'Nabokov',
+    pagesCount: 200,
+    language: 'Russian',
+    genre: 'fiction',
+  },
+  {
+    title: 'Deep into Universe',
+    author: 'Hawking',
+    pagesCount: 100,
+    language: 'English',
+    genre: 'non-fiction',
+  },
+  {
+    title: 'The old man and the sea',
+    author: 'Hemingway',
+    pagesCount: 70,
+    language: 'English',
+    genre: 'fiction',
+  },
+  {
+    title: 'The Great Gatsby',
+    author: 'Fitzgerald',
+    pagesCount: 220,
+    language: 'English',
+    genre: 'fiction',
+  },
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService extends Dexie {
+  public books!: Table<Book, number>;
+  public authors!: Table<Author, number>;
+
+  constructor() {
+    super('ngdexieliveQuery');
+    this.version(3).stores({
+      books: '++id',
+      authors: '++id, &name',
+    });
+    this.on('populate', () => this.populate());
+  }
+
+  async populate() {
+    try {
+      await this.books.bulkAdd(BOOKS);
+      await this.books.each((book) => this.authors.add({name: book.author}));
+    }
+    catch (e) {
+      throw new Error(`DataService populate error: ${e}`);
+    }
+  }
+
 @Injectable({
   providedIn: 'root'
 })
@@ -83,10 +147,30 @@ export class DataService extends Dexie {
   }
 
   public async addBook(book: Book) {
-    this.books.add(book);
-    const author = await this.authors.get({name: book.author});
-    if (!author) {
-      this.authors.add({name: book.author});
+    try {
+      await this.books.add(book);
+      const author = await this.authors.get({name: book.author});
+      if (!author) {
+        await this.authors.add({name: book.author});
+      }
+    }
+    catch (e) {
+      throw new Error(`DataService addBook error: ${e}`);
+    }
+  }
+
+  public async addBooks(books: Book[]) {
+    try {
+      await this.books.bulkAdd(books);
+      for (const book of books) {
+        const author = await this.authors.get({name: book.author});
+        if (!author) {
+          await this.authors.add({name: book.author});
+        }
+      }
+    }
+    catch (e) {
+      throw new Error(`DataService addBooks error: ${e}`);
     }
   }
 
@@ -96,13 +180,23 @@ export class DataService extends Dexie {
   }
 
   public async addAuthor(name: Book[`author`]) {
-    const author = await this.authors.get({name});
-    if (!author) {
-      this.authors.add({name});
+    try {
+      const author = await this.authors.get({name});
+      if (!author) {
+        await this.authors.add({name});
+      }
+    }
+    catch (e) {
+      throw new Error(`DataService addAuthor error: ${e}`);
     }
   }
 
-  public editAuthor(author: Book[`author`], newAuthor: Book[`author`]) {
-    this.authors.where({name: author}).modify({name: newAuthor});
+  public async editAuthor(author: Book[`author`], newAuthor: Book[`author`]) {
+    try {
+      await this.authors.where({name: author}).modify({name: newAuthor});
+    }
+    catch (e) {
+      throw new Error(`DataService editAuthor error: ${e}`);
+    }
   }
 }
